2b447565115cefbbb0fb736bb531b42fc83c3ed2
Juan Carlos Mellizo <juancho@MacBook-Pro-de-Juan.local>
2017-08-21 02:09:55 -0500

    Examen2Corregido

--- Examenes/Examen2/examen2.js
+++ Examenes/Examen2/examen2.js
@@ -92,7 +92,6 @@ class Utilidades{
 	static generarPaisAleatorio(){
 		let nombresPaises = ["Colombia", "Corea Norte", "EEUU", "Rusia", "Venezuela", "España", "China", "Iran", "Portugal", "Mexico", "Brasil"];
 		let indice = this.generarNumeroAleatorioEntre(0, nombresPaises.length-1);
-		nombresPaises.splice(indice,1);
 	return nombresPaises[indice];
 	}
 
@@ -100,49 +99,60 @@ class Utilidades{
 
 
 class Soldado{
-	constructor(){
+	constructor(potencia){
 		this._nombre = Utilidades.generarNombreAleatorio();
 		this._salud = 100;
-		this._potenciaDeAtaque = 0;
+		this._potenciaDeAtaque = potencia;
 	}
 
 	ataca(soldado){
-		soldado._salud = soldado._salud - this._potenciaDeAtaque;
+		soldado.restarSalud(this._potenciaDeAtaque);
+	}
+
+	restarSalud(saludARestar){
+		this._salud -= saludARestar; 
+		if (this._salud <=0 ){
+			this._salud = 0;
+		}
+
+		return this._salud;
 	}
 }
 
 
 class SoldadoDeInfanteria extends Soldado{
 	constructor(){
-		super();
-		this._potenciaDeAtaque = Utilidades.generarNumeroAleatorioEntre(1,25);
+		//this._potenciaDeAtaque = Utilidades.generarNumeroAleatorioEntre(1,25);
+		let potencia = Utilidades.generarNumeroAleatorioEntre(1,25);
+		super(potencia);
 	}
 }
 
 class SoldadoDeCaballeria extends Soldado{
 	constructor(){
-		super();
-		this._potenciaDeAtaque = Utilidades.generarNumeroAleatorioEntre(25,50);
+		let potencia = Utilidades.generarNumeroAleatorioEntre(25,50);
+		super(potencia);
 	}
 }
 
 class SoldadoDeArtilleria extends Soldado{
 	constructor(){
-		super();
-		this._potenciaDeAtaque = Utilidades.generarNumeroAleatorioEntre(50,75);
+		let potencia = Utilidades.generarNumeroAleatorioEntre(50,75);
+		super(potencia);
 	}
 }
 
 class SoldadoPilotoF18 extends Soldado{
 	constructor(){
-		super();
-		this._potenciaDeAtaque = Utilidades.generarNumeroAleatorioEntre(75,100);
+		let potencia = Utilidades.generarNumeroAleatorioEntre(75,100);
+		super(potencia);
 	}
 }
 
 class Ejercito{
 	constructor(){
 		this._pais = Utilidades.generarPaisAleatorio();
+		//this._pais = pais;
 		this._soldados = [];
 		this._bajas = [];
 		this.convocarEjercito();
@@ -169,6 +179,14 @@ class Ejercito{
 			this._soldados.push(soldadoPilotoF18);
 		}
 	}
+
+	getNumeroSoldadosVivos(){
+		return this._soldados.length;
+	}
+
+	getNumeroSoldadosMuertos(){
+		return this._bajas.length;
+	}
 }
 
 class Guerra{
@@ -176,15 +194,43 @@ class Guerra{
 		this._numeroJornadastrans = 0;
 		this._ejercito1 = ejercito1; 
 		this._ejercito2 = ejercito2;
+		this._idInterval = null;
 	}
 
-	iniciarGuerra(){
-		this.jornadaDeGuerra();
+	iniciarGuerra(guerra) {
+	  this._idInterval = window.setInterval(function(){guerra.crearIntervaloDeUnSegundo()}, 1000);
 	}
+
+	crearIntervaloDeUnSegundo(){
+		this.ejecutarJornada();	
+	}
+
+
+	ejecutarJornada(){
+		this._numeroJornadastrans ++;
+		this.imprimirEstado();
+		this.jornadaDeGuerra();
+		if(this._ejercito1.getNumeroSoldadosVivos() <= 0 || this._ejercito2.getNumeroSoldadosVivos ()<= 0){
+ 			window.clearInterval(this._idInterval);  
+ 		}
+
+	}
+
+	imprimirEstado(){
+		
+		//console.clear();
+		console.log("%cInicia la guerra", "color: red; background-color: yellow");
+		console.log ("numero de jornadas transcurriadas :" + this._numeroJornadastrans);
+		console.log ("Soldados Vivos Ejercito 1: " +  this._ejercito1.getNumeroSoldadosVivos());
+		console.log ("Soldados Vivos Ejercito 2: "+ this._ejercito2.getNumeroSoldadosVivos());
+		console.log ("Bajas ejercito1: " + this._ejercito1.getNumeroSoldadosMuertos());
+		console.log ("Bajas ejercito2: " + this._ejercito2.getNumeroSoldadosMuertos());
+	}
+
 
 	jornadaDeGuerra(){
 
-
+		//debugger;
 		// Ataca Ejercito 2
 		for (let i=0; i < this._ejercito2._soldados.length; i++){
 			let soldadoEj2 = this._ejercito2._soldados[i];
@@ -222,43 +268,17 @@ class Guerra{
 	}
 
 	seleccionaObjetivo(ejercito){
-// busca en el array del ejercito contrario para traer el objetivo
-	// let nombresSoldados = ["Bryan", "Spartacus", "Cesar", "Ospina", "Colón", "Crisus", "Hulk", "Capitan America", "Batman", "Maradona", "Pele"];
-	// let indice = Utilidades.generarNumeroAleatorioEntre(0, nombresSoldados.length-1);
-	// return nombresSoldados[indice];
-
-	let indice = Utilidades.generarNumeroAleatorioEntre(0, ejercito._soldados.length-1);
-	return ejercito._soldados[indice];
-
+		let indice = Utilidades.generarNumeroAleatorioEntre(0, ejercito._soldados.length-1);
+		return ejercito._soldados[indice];
 	}
 }
 
-
-var IDInterval;
-var ejercito1 = new Ejercito();
-var ejercito2 = new Ejercito();
-
 window.onload=function(){
-let guerra = new Guerra(ejercito1, ejercito2);
-console.log("Inicia la guerra");
-	IDInterval = window.setInterval(function(){
-		if (ejercito1._bajas.length == 1000 ||  ejercito2._bajas.length == 1000){
-			clearInterval(IDInterval);  
-		}
-		else{
-			guerra._numeroJornadastrans ++;
-			guerra.iniciarGuerra();
-
-			console.log ("numero de jornadas transcurriadas :" + guerra._numeroJornadastrans);
-			console.log ("Soldados Vivos Ejercito 1: " + ejercito1._soldados.length);
-			console.log ("Soldados Vivos Ejercito 2: "+ ejercito2._soldados.length);
-			console.log ("Bajas ejercito1: " + ejercito1._bajas.length);
-			console.log ("Bajas ejercito2: " + ejercito2._bajas.length);
-		}
-	}, 1000);
-}
-
-// detengo el interval clearInterval(IDInterval).
+	let ejercito1 = new Ejercito();
+	let ejercito2 = new Ejercito();
+	let guerra = new Guerra(ejercito1, ejercito2);
+	guerra.iniciarGuerra(guerra);
+}	
 
 
 
